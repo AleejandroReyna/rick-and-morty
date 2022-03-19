4637371a320e1a58001c20f477308ca874d710e3
Alejandro Reyna <AleejandroReyna@gmail.com>
2022-03-19 11:15:02 -0600

    Router init (#1)
    * router-init: Configure deno init
    
    * router-init: configure init deno depts
    
    * router-init: Configure init dotenv file
    
    * router-init: Created .env.example file
    
    * router-init: Added first handler for home page
    
    * router-init: Configure router with first route
    
    * router-init: Set router on application

---
+++ server.ts
@@ -0,0 +1,22 @@
+import {
+    Application,
+    dotenv
+} from './depts.ts'
+
+
+import { Router } from "./config/router.ts"
+
+// Get config enviroment variables
+const { PORT } = dotenv.config();
+
+// Define server App
+const app : Application = new Application();
+
+// Get and Parse Port number for server
+const port  = Number(PORT);
+
+// Set router and allowed Methods
+app.use(Router.routes());
+app.use(Router.allowedMethods());
+
+await app.listen({ port });
