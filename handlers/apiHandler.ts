f5a69336ad07ee67c699760c6bd5db96d85c6236
Alejandro Reyna <AleejandroReyna@gmail.com>
2022-03-21 00:25:17 -0600

    api-requests: Try functionality for DataManagers on api handler

--- handlers/apiHandler.ts
+++ handlers/apiHandler.ts
@@ -1,10 +1,22 @@
-import { Response } from "../depts.ts";
+import { Response } from "../depts.ts"
+import { DataManager } from '../services/managers/DataManager.ts'
 
 // Route for Home page 
-export const getInfo = ({ response }: { response : Response }) => {
+export const getInfo = async ({ response } : { response : Response }) => {
+    
+    // create managers
+    const characterManager = new DataManager('character', 826)
+    const locationManager = new DataManager('location', 126)
+    const episodeManager = new DataManager('episode', 51)
+
+    //get data from managers
+    const locationData = await locationManager.getData()
+    const characterData = await characterManager.getData()
+    const episodeData = await episodeManager.getData()
+    
     response.status = 200
     response.body = {
-        test: 'working'
+        status: 'working'
     }
     return response
 };
