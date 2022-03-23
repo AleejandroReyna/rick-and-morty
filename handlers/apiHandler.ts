5b522dc03b079b301ff03ce281db0451bf7f6a50
Alejandro Reyna <AleejandroReyna@gmail.com>
2022-03-22 21:16:51 -0600

    second-challenge: convert on single line getInfo function

--- handlers/apiHandler.ts
+++ handlers/apiHandler.ts
@@ -1,24 +1,22 @@
 import { Response } from "../depts.ts"
-import { DataManager } from '../services/managers/DataManager.ts'
-import { ResponseManager } from '../services/managers/ResponseManager.ts'
 import { CountExerciseManager} from '../services/managers/CountExerciseManager.ts'
 import { ICountExerciseParams} from '../services/managers/interfaces.ts'
 
 // Route for Home page 
-export const getInfo = async ({ response } : { response : Response }) => {
+export async function getInfo({ response }: { response: Response; }) {
 
-    // create count exercise
-    const countExerciseParams : ICountExerciseParams[] = [
-        {resource: 'character', total: 826, search: 'c'},
-        {resource: 'location', total: 126, search: 'l'},
-        {resource: 'episode', total: 51, search: 'e'},
-    ]
-    const countExercise = new CountExerciseManager('Char counter', countExerciseParams)
-    const countExerciseResult = await countExercise.executeExercise()
-    
-    response.status = 200
-    response.body = [
-        countExerciseResult
-    ]
-    return response
-};
+  // create count exercise
+  const countExerciseParams: ICountExerciseParams[] = [
+    { resource: 'character', total: 826, search: 'c' },
+    { resource: 'location', total: 126, search: 'l' },
+    { resource: 'episode', total: 51, search: 'e' },
+  ];
+  const countExercise = new CountExerciseManager('Char counter', countExerciseParams);
+  const countExerciseResult = await countExercise.executeExercise();
+
+  response.status = 200;
+  response.body = [
+    countExerciseResult
+  ];
+  return response;
+}
