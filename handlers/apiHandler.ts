24b0927511e1357a80a769072db48b8cbbf4bb92
Alejandro Reyna <AleejandroReyna@gmail.com>
2022-03-22 21:09:21 -0600

    first-challenge: Added an Instance of Count Exercise manager to api handler

--- handlers/apiHandler.ts
+++ handlers/apiHandler.ts
@@ -1,29 +1,24 @@
 import { Response } from "../depts.ts"
 import { DataManager } from '../services/managers/DataManager.ts'
 import { ResponseManager } from '../services/managers/ResponseManager.ts'
+import { CountExerciseManager} from '../services/managers/CountExerciseManager.ts'
+import { ICountExerciseParams} from '../services/managers/interfaces.ts'
 
 // Route for Home page 
 export const getInfo = async ({ response } : { response : Response }) => {
-    
-    // create data managers
-    const characterManager = new DataManager('character', 826)
-    const locationManager = new DataManager('location', 126)
-    const episodeManager = new DataManager('episode', 51)
 
-    // create response managers
-    const responseCharacterManager = new ResponseManager(characterManager)
-    const responseLocationManager = new ResponseManager(locationManager)
-    const responseEpisodeManager = new ResponseManager(episodeManager)
-
-    const results = await Promise.all([
-        responseCharacterManager.generateResponse("c"),
-        responseLocationManager.generateResponse("l"),
-        responseEpisodeManager.generateResponse("e")
-    ])
+    // create count exercise
+    const countExerciseParams : ICountExerciseParams[] = [
+        {resource: 'character', total: 826, search: 'c'},
+        {resource: 'location', total: 126, search: 'l'},
+        {resource: 'episode', total: 51, search: 'e'},
+    ]
+    const countExercise = new CountExerciseManager('Char counter', countExerciseParams)
+    const countExerciseResult = await countExercise.executeExercise()
     
     response.status = 200
-    response.body = {
-        results
-    }
+    response.body = [
+        countExerciseResult
+    ]
     return response
 };
