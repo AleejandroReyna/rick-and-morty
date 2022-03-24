3473403927d1250ebc57739a6c4a581e99ea0b33
Alejandro Reyna <AleejandroReyna@gmail.com>
2022-03-23 22:14:32 -0600

    Second challenge (#7)
    * second-challenge: convert on single line getInfo function
    
    * second-challenge: Change in_time parameter for count exercise manager
    
    * second-challenge: Fixed problem for Data Manager
    
    Error was in fetch Data method
    
    * second-challenge: Created Episode Location Manager
    
    * second-challenge: Added second challenge in api handler

--- handlers/apiHandler.ts
+++ handlers/apiHandler.ts
@@ -1,24 +1,31 @@
 import { Response } from "../depts.ts"
-import { DataManager } from '../services/managers/DataManager.ts'
-import { ResponseManager } from '../services/managers/ResponseManager.ts'
 import { CountExerciseManager} from '../services/managers/CountExerciseManager.ts'
+import { EpisodeLocationsManager } from '../services/managers/EpisodeLocationsManager.ts'
 import { ICountExerciseParams} from '../services/managers/interfaces.ts'
 
 // Route for Home page 
-export const getInfo = async ({ response } : { response : Response }) => {
+export async function getInfo({ response }: { response: Response; }) {
 
-    // create count exercise
-    const countExerciseParams : ICountExerciseParams[] = [
-        {resource: 'character', total: 826, search: 'c'},
-        {resource: 'location', total: 126, search: 'l'},
-        {resource: 'episode', total: 51, search: 'e'},
-    ]
-    const countExercise = new CountExerciseManager('Char counter', countExerciseParams)
-    const countExerciseResult = await countExercise.executeExercise()
-    
-    response.status = 200
-    response.body = [
-        countExerciseResult
-    ]
-    return response
-};
+  // create count exercise
+  const countExerciseParams: ICountExerciseParams[] = [
+    { resource: 'character', total: 826, search: 'c' },
+    { resource: 'location', total: 126, search: 'l' },
+    { resource: 'episode', total: 51, search: 'e' },
+  ];
+  const countExercise = new CountExerciseManager('Char counter', countExerciseParams);
+  const episodeLocationsParams = {
+    list: {resource: 'episode', total: 51},
+    through: {resource: 'character', total: 826}
+  }
+  const episodeLocationsExercise = new EpisodeLocationsManager('Episode Locations', episodeLocationsParams)
+
+  const results = [
+    await countExercise.executeExercise(),
+    await episodeLocationsExercise.executeExercise()
+  ]
+  
+
+  response.status = 200;
+  response.body = results
+  return response;
+}
