d452088a1e497fbb13c38678b4b23994a4a0aca0
Alejandro Reyna <AleejandroReyna@gmail.com>
2022-03-23 22:12:44 -0600

    second-challenge: Fixed problem for Data Manager
    Error was in fetch Data method

--- services/managers/DataManager.ts
+++ services/managers/DataManager.ts
@@ -13,8 +13,8 @@ export class DataManager implements IManager{
     fetchData = () => {
         let pageTotal : number = this.total / 20
         this.total % 20 && pageTotal++
-        const promises = Array.from({length: pageTotal}, async (_, i) => {
-            const request = await apiRequest(this.resource, i)
+        const promises = Array.from({length: Math.floor(pageTotal)}, async (_, i) => {
+            const request = await apiRequest(this.resource, i + 1)
             return request.json()
         })
         return Promise.all(promises)
