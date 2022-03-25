eb40b9c7aa9e4201e245591cdd99c78d54c83d96
Alejandro Reyna <AleejandroReyna@gmail.com>
2022-03-25 01:04:29 -0600

    improving-challenges: Set interfaces on manager data

--- services/managers/DataManager.ts
+++ services/managers/DataManager.ts
@@ -1,4 +1,4 @@
-import { IManager} from './interfaces.ts'
+import { IManager, ICharacter, IResponse, ILocation, IEpisode } from './interfaces.ts'
 import { apiRequest } from '../requests/apiRequest.ts'
 
 export class DataManager implements IManager{
@@ -10,21 +10,21 @@ export class DataManager implements IManager{
         this.total = total
     }
     
-    fetchData = () => {
+    fetchData = () : Promise<IResponse[]> => {
         let pageTotal : number = this.total / 20
         this.total % 20 && pageTotal++
         const promises = Array.from({length: Math.floor(pageTotal)}, async (_, i) => {
             const request = await apiRequest(this.resource, i + 1)
             return request.json()
         })
-        return Promise.all(promises)
+        const promise = Promise.all(promises)
+        return promise
     }
 
-    getData = async () => {
+    getData = async () : Promise<(ICharacter|ILocation|IEpisode)[]> => {
         try {
-            const request = await this.fetchData()
-            const data = [].concat(...request.map(item => item.results))
-            return data
+            const dataset = await this.fetchData()
+            return dataset.map(item => item.results).flat()
         } catch(e) {
             throw e
         }
