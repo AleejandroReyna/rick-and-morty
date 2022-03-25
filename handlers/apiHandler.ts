efeeddddd0b8859bd144b57696dd8bb79974503c
Alejandro Reyna <AleejandroReyna@gmail.com>
2022-03-25 01:35:48 -0600

    improving-challenges: Changed api handler
    Get data first and execute functions with gotten data

--- handlers/apiHandler.ts
+++ handlers/apiHandler.ts
@@ -1,29 +1,45 @@
 import { Response } from "../depts.ts"
 import { CountExerciseManager} from '../services/managers/CountExerciseManager.ts'
 import { EpisodeLocationsManager } from '../services/managers/EpisodeLocationsManager.ts'
-import { ICountExerciseParams} from '../services/managers/interfaces.ts'
+import { 
+  IEpisodeLocationsParams
+ } from '../services/managers/interfaces.ts'
+import { DataManager } from '../services/managers/DataManager.ts'
 
 // Route for Home page 
 export async function getInfo({ response }: { response: Response; }) {
 
+  const startTime = performance.now()
+
+  const params = [{ resource: 'character', total: 826},
+                  { resource: 'location', total: 126},
+                  { resource: 'episode', total: 51}]
+
+  const [characterData, locationData, episodeData] = await Promise.all(
+    params.map(param => {
+      const manager = new DataManager(param.resource, param.total)
+      return manager.getData()
+    })
+  )
+
   // create count exercise
-  const countExerciseParams: ICountExerciseParams[] = [
-    { resource: 'character', total: 826, search: 'c' },
-    { resource: 'location', total: 126, search: 'l' },
-    { resource: 'episode', total: 51, search: 'e' },
+  const countExerciseParams  = [
+    { data: characterData, search: 'c' },
+    { data: locationData, search: 'l' },
+    { data: episodeData, search: 'e' },
   ];
+
   const countExercise = new CountExerciseManager('Char counter', countExerciseParams);
-  const episodeLocationsParams = {
-    list: {resource: 'episode', total: 51},
-    through: {resource: 'character', total: 826}
+  const episodeLocationsParams : IEpisodeLocationsParams = {
+    list: episodeData,
+    through: characterData
   }
   const episodeLocationsExercise = new EpisodeLocationsManager('Episode Locations', episodeLocationsParams)
 
-  const results = [
-    await countExercise.executeExercise(),
-    await episodeLocationsExercise.executeExercise()
-  ]
-  
+  const results = await Promise.all([
+    countExercise.executeExercise(startTime),
+    episodeLocationsExercise.executeExercise(startTime)
+  ])
 
   response.status = 200;
   response.body = results
